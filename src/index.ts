d0466cd96ebaf6642a340a07b7e1a905da538d53
Jan Eckerlein <eckerlein.jan@gmail.com>
2024-11-21 19:15:17 +0100

    fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 type Keys = (string | number)[];
-type Obj = Record<string | symbol | number, any>;
+type Obj = Record<string | symbol | number, unknown>;
 
-export function setOnPath(obj: Obj, path: string, value: any) {
+export function setOnPath(obj: Obj, path: string, value: unknown) {
   const keys = getPathKeys(path);
   recursiveSet(obj, keys, value);
 }
 
-export function recursiveSet(ref: Obj, keys: Keys, value: any) {
+export function recursiveSet(ref: Obj, keys: Keys, value: unknown) {
   const key = keys[0];
   if (keys.length > 1) {
     const nextKey = keys[1];
 
     if (typeof nextKey === 'number' && !Array.isArray(ref[key])) {
-      ref[key] = new Array();
+      ref[key] = [];
     } else if (typeof nextKey === 'string' && !isObject(ref[key])) {
       ref[key] = {};
     }
     const newKeys = keys.slice(1);
-    recursiveSet(ref[key], newKeys, value);
+    recursiveSet(ref[key] as Obj, newKeys, value);
   } else {
     ref[key] = value;
   }
 }
 
-function isObject(x: any) {
+function isObject(x: unknown) {
   return typeof x === 'object' && !Array.isArray(x) && x !== null;
 }
 
